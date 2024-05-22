a355bb6114ea57d7a4400eb4ae50b55631138ea7
Christian <kristian2015ok@gmail.com>
2024-05-22 14:52:26 +0700

    Add: Configuration

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => [   
+        "first" => env("NAME_FIRST", "Chris"),
+        "last" => env("NAME_LAST", "Tian")
+    ],
+    "email" => "kristian2015ok@gmail.com",
+    "web" => "https://www.linkedin.com/in/christian72/"
+];
