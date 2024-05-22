bddbf5b17a1b994dca62d9e7dd3e68f04530adbe
Christian <kristian2015ok@gmail.com>
2024-05-22 14:21:32 +0700

    Add: Testing

---
+++ tests/Unit/ContohTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ContohTest extends TestCase
+{
+    // php artisan make:test ContohTest --unit
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
