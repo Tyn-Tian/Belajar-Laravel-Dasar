6448a782e0e588f3d5987916f6e9e0b2985e3737
Christian <kristian2015ok@gmail.com>
2024-05-22 20:54:35 +0700

    Add: Service Provider

---
+++ tests/Feature/FooBarServiceProviderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use App\Services\HelloService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FooBarServiceProviderTest extends TestCase
+{
+    public function testServiceProvider()
+    {
+        $foo = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertSame($foo, $foo2);
+
+        $bar = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertSame($bar, $bar2);
+
+        self::assertSame($foo, $bar->foo);
+        self::assertSame($foo2, $bar2->foo);
+    }
+
+    public function testPropertySingleton()
+    {
+        $helloService = $this->app->make(HelloService::class);
+        $helloService1 = $this->app->make(HelloService::class);
+
+        self::assertSame($helloService, $helloService1);
+
+        self::assertEquals("Hallo Tian", $helloService->hello("Tian"));
+    }
+}
